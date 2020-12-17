6d86c661dd7ff64367acd96bbd69e994a9cfcf39
hota1024 <hotalog@hota1024.com>
2020-12-17 11:02:57 +0900

    Initial Commit

---
+++ anikaku/anikakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  anikakuApp.swift
+//  anikaku
+//
+//  Created by 納本穂高 on 2020/12/17.
+//
+
+import SwiftUI
+
+@main
+struct anikakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
